a792a0e6f7006bda7eb2d4077c83c5b8f4aed5ff
Paul Mathieu <56612315+paul-mathieu@users.noreply.github.com>
2019-11-09 23:48:15 +0100

    Update math.py

--- STID/math.py
+++ STID/math.py
@@ -101,8 +101,54 @@ def nPrimeNumbersEasy(n):
 
 
 
+#
+#==============================================================================
+#  
+#==============================================================================
+#
 
 
+# One Line script
+
+def exponentiationLists_v1(n):
+    return [list(map(lambda x: x(r), [lambda x: x**2, lambda x: x**3]))) for r in range(n)]
+
+#you can also do :
+
+def exponentiationLists_v2(n):
+    return [[x**p for p in [1,2]] for x in range(10)]
+
+#for specifical exponentiation you can do :
+
+def exponentiationLists_v3(n, listE):
+    return [[x**p for p in listE] for x in range(10)]
+
+
+# Normal Script
+
+def exponentiationListsEasy(n, listE):
+    
+    #principal list
+    lst = []
+    
+    for number in range(n):
+        
+        #lists for exponentiation
+        tempLst = []
+        
+        for expo in listE:
+            #add the number
+            tempLst.append(number**expo)
+            
+        #add the list of numbers
+        lst.append(tempLst)
+        
+    return lst
+            
+
+# Tests
+
+#exponentiationLists_v3(10, [1,2])
 
 
 

8f578c2e6888b996a6033814e38017282bb69844
Paul Mathieu <56612315+paul-mathieu@users.noreply.github.com>
2019-11-10 00:17:22 +0100

    Update sentence.py

--- STID/sentence.py
+++ STID/sentence.py
@@ -34,7 +34,7 @@ def repeatSentenceEasy(sentence, number):
 
 #
 #==============================================================================
-#  
+#  Convert a sentence in Camelcase
 #==============================================================================
 #
 
@@ -70,7 +70,70 @@ def sentenceToCamelEasy(phrase):
 
 
 
+#
+#==============================================================================
+#  Palindrome
+#==============================================================================
+#
 
+
+# One Line script
+
+def isPalindrome_v1(string):
+    return string == string[::-1]
+
+def isPalindrome_v2(string):
+    #sum of boolean values : True == 1 and False == 0
+    return sum([string[index] != string[len(string) - (index + 1)] for index in range(len(string) - 1)]) == 0
+
+# Normal Script
+
+def isPalindromeEasy(string):
+    #for each element of an half string ('semâmes' -> 'sem')
+    for index in range(len(string) - 1):
+        #if it's the same that the opposit element
+        if string[index] != string[len(string) - (index + 1)]:
+            return False
+    return True
+
+# Tests
+
+#isPalindrome_v2('semâmes')
+
+
+
+#
+#==============================================================================
+#  Number of different letters between two strings
+#==============================================================================
+#
+
+
+# One Line script
+
+def marteau(str1, str2):
+    return sum(x != y for x, y in zip(str1, str2)) if len(str1) == len(str2) else 'error'
+
+
+# Normal Script
+
+def marteauEasy(str1, str2):
+    
+    #if the words aren't the same length
+    if len(str1) == len(str2):
+        return 'error'
+    
+    diffLetters = 0
+    for index in range(str1):
+        if str1[index] != str2[index]:
+            diffLetters += 1
+    
+    return diffLetters
+    
+
+# Tests
+
+#marteau('semâmes', 'semions')
 
 
 

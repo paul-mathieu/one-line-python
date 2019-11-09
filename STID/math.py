feb1f367f79655d94bb5140187a4e9cb7f5f8ac0
Paul Mathieu <56612315+paul-mathieu@users.noreply.github.com>
2019-11-09 15:21:31 +0100

    Create math.py

---
+++ STID/math.py
@@ -0,0 +1,13 @@
+"""
+Author : Paul Mathieu (/paul-mathieu)
+
+In this file, you can find Python programs in one line to play with strings.
+All of these programs were done during my first year of learning Python.
+
+"""
+
+
+
+
+
+

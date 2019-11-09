30a4357b70263ed35c7fdc2abbc61a25b771ceaf
Paul Mathieu <56612315+paul-mathieu@users.noreply.github.com>
2019-11-09 17:20:22 +0100

    Update sentence.py

--- STID/sentence.py
+++ STID/sentence.py
@@ -41,13 +41,34 @@ def repeatSentenceEasy(sentence, number):
 
 # One Line script
 
+def sentenceToCamel(phrase):
+    return "".join(x + y for x, y in zip(phrase[::2].upper(), phrase[1::2].lower()))
 
 
 # Normal Script
 
+def sentenceToCamelEasy(phrase):
+    
+    #initialization of the variable that will be returned
+    string = ""
+    
+    #for each element of the string,
+    #if it is even, it will be returned in upper case
+    #else it will be returned in lower case
+    for k in range(len(phrase)):
+        if k%2 == 0:
+            string += phrase[k].upper()
+        else:
+            string += phrase[k].lower()
+    
+    return string
 
 
 # Tests
+
+#print(sentenceToCamel('My name is Paul'))
+
+
 
 
 

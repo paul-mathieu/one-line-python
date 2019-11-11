a8ed13580d5947b0c8abc8ce2f54c4f30853f6ef
Paul Mathieu <56612315+paul-mathieu@users.noreply.github.com>
2019-11-11 22:47:40 +0100

    Delete dictLettersNumbers_justOL.py

--- STID/dictLettersNumbers_justOL.py
+++
@@ -1,2 +0,0 @@
-def dictLettersNumbers(phrase):
-    return {"numbers" : reduce(lambda x, y : x + y, list(map(lambda x: x.isdigit(),list(phrase)))), "letters" : reduce(lambda x, y : x + y, list(map(lambda x: x.isalpha(),list(phrase))))}
